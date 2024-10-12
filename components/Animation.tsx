79064a189849484bccf67cd63ad01007dccb3611
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-12 21:05:14 +0100

    working on the animation

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -57,6 +57,25 @@ const animation = () => {
         <div className="cell size-2"></div>
         <div className="cell size-14"></div>
         {/* <div className="cell size-6 "></div> */}
+        <div className="cell size-5"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-9"></div>
+      </div>
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-8 pink"></div>
+        <div className="cell size-4"></div>
+        {/* <div className="cell size-5"></div> */}
+        <div className="cell size-8 beige"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-14"></div>
+        {/* <div className="cell size-6 "></div> */}
         <div className="cell size-22"></div>
       </div>
      
