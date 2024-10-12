88d870d7705e49a461b6240fb2003bc33d8c43c1
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-12 21:28:36 +0100

    working on the animation

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -78,7 +78,147 @@ const animation = () => {
         {/* <div className="cell size-6 "></div> */}
         <div className="cell size-22"></div>
       </div>
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-12"></div>
+        <div className="cell size-8 pink"></div>
+       
+        <div className="cell size-12"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-14"></div>
+        {/* <div className="cell size-6 "></div> */}
+        <div className="cell size-22"></div>
+      </div>
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-14"></div>
+        <div className="cell size-4 pink"></div>
+       
+        <div className="cell size-14"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-14"></div>
+        {/* <div className="cell size-6 "></div> */}
+        <div className="cell size-22"></div>
+      </div>
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-4"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-15"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-14"></div>
+        {/* <div className="cell size-6 "></div> */}
+        <div className="cell size-22"></div>
+      </div>
      
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-4"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-15"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-14"></div>
+        {/* <div className="cell size-6 "></div> */}
+        <div className="cell size-22"></div>
+      </div>
+     
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-4"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-15"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-14"></div>
+        {/* <div className="cell size-6 "></div> */}
+        <div className="cell size-22"></div>
+      </div>
+     
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-4"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-15"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-14"></div>
+        {/* <div className="cell size-6 "></div> */}
+        <div className="cell size-22"></div>
+      </div>
+     
+      <div className="row">
+        <div className="cell size-23"></div>
+        <div className="cell "></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-4"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-15"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-1 purple"></div>
+        <div className="cell size-2 purple"></div>
+        <div className="cell size-9"></div>
+        <div className="cell size-6 beige"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-6 "></div>
+        <div className="cell size-11"></div>
+      </div>
+      <div className="row">
+        <div className="cell size-23"></div>
+        <div className="cell size-6"></div>
+        <div className="cell "></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-4"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-15"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-1 purple"></div>
+        <div className="cell size-2 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-7 beige"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-4 "></div>
+        <div className="cell size-3 "></div>
+        <div className="cell size-11"></div>
+      </div>
+      <div className="row">
+        <div className="cell size-4"></div>
+        <div className="cell size-10"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-18"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-23"></div>
+        <div className="cell size-10"></div>
+        <div className="cell size-7"></div>
+      </div>
     </div>
   );
 };
