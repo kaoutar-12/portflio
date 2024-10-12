df2ef8fbe6b2fb43883357a601d55361ee5aaf68
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-12 19:10:59 +0100

    working on the animation

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -5,16 +5,44 @@ const animation = () => {
   return (
     <div className="container">
       <div className="row">
+        <div className="cell size-4"></div>
+        <div className="cell size-10"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-18"></div>
         <div className="cell size-5"></div>
-        <div className="cell size-18"></div>
-        <div className="cell size-18"></div>
-        <div className="cell size-18"></div>
-        <div className="cell size-18"></div>
-        <div className="cell size-18"></div>
-        <div className="cell size-18"></div>
-        <div className="cell size-18"></div>
-        <div className="cell size-18"></div>
+        <div className="cell size-7"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-23"></div>
+        <div className="cell size-10"></div>
+        <div className="cell size-7"></div>
       </div>
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-10"></div>
+        <div className="cell size-9 darkpurple"></div>
+        <div className="cell size-17"></div>
+        <div className="cell size-5"></div>
+        <div className="cell size-9 purple"></div>
+        <div className="cell size-2"></div>
+        <div className="cell size-23"></div>
+        <div className="cell size-6 pink"></div>
+        <div className="cell size-23"></div>
+      </div>
+      <div className="row">
+        <div className="cell size-22"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-5 darkpurple"></div>
+        <div className="cell size-8 pink"></div>
+        <div className="cell size-13"></div>
+        {/* <div className="cell size-5"></div> */}
+        <div className="cell size-8 beige"></div>
+        <div className="cell size-5 purple"></div>
+        <div className="cell size-6"></div>
+        <div className="cell size-18"></div>
+        <div className="cell size-6 pink"></div>
+        <div className="cell size-20"></div>
+      </div>
+     
     </div>
   );
 };
