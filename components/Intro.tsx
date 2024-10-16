db3ca6b430840e12cfa22523491bebc9c63c91d1
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-16 17:57:25 +0100

    working on the animation

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,17 +1,37 @@
-  import React from 'react'
+import React from "react";
 import "@/styles/intro.css";
 
 const Intro = () => {
   return (
-    <div>
-        <section className="intro">
-            <div className="top">
-              
+    <section className="intro">
+      <div className="info">
+        <div className="header">
+          <div className="avatar"></div>
+          <div className="texts">
+            <div className="name">
+              <div>
+                Hello World! My name <span>Kaoutar Mouradi</span>
+              </div>
             </div>
-            <div className="bottom"></div>
-        </section>
-    </div>
-  )
-}
+            <div className="job">
+              <div>Front-end Developer</div>
+            </div>
+            <div className="description">
+              <p>
+                I'm a front-end developer based in Morocco. I have serious
+                passion for UI effects, animations and creating intuitive,
+                dynamic user experiences.
+              </p>
+              </div>  
+          </div>
+        </div>
+        <div className="tags">
+          
+        </div>
+      </div>
+      <div className="icon"></div>
+    </section>
+  );
+};
 
-export default Intro
+export default Intro;
