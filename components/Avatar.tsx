f592fedffb824feaf3d94d90cbed91f3f2a11e82
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-20 12:00:42 +0100

    start working on contact section

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,10 +1,18 @@
-
+'use client';
+import { useState } from "react";
 
 import React from "react";
 import "@/styles/contact.css";
 import Image from "next/image";
 
 const Avatar = () => {
+
+  const [hover, setHover] = useState(false);
+
+  const handleHover = () => {
+    setHover(!hover);
+  }
+
   return (
     <div className="contact-section">
       <div className="contact">
@@ -14,11 +22,25 @@ const Avatar = () => {
           width={200}
           height={200}
           className="contact-pic"
+          onClick={handleHover}
         />
         <div className="hover-content">hello! Click me to contact</div>
         <div className="first-bubble"></div>
         <div className="second-bubble"></div>
         <div className="third-bubble"></div>
+     
+        {hover && (
+          <div className="clicked-content">
+           <div className="github">
+           <span className="skill-icons--github-dark"></span>
+           </div>
+           <div className="linkdin">
+           <span className="devicon--linkedin"></span>
+           </div>
+           <div className="instagram"><span className="skill-icons--instagram"></span></div>
+           <div className="email"><span className="skill-icons--gmail-dark"></span></div>
+          </div>
+        )}
       </div>
     </div>
   );
